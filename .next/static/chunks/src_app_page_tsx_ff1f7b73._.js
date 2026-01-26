f5303db84988d414abf962404da6dca6026d8c55
shoriful-dev <cmshohelrana50@gmail.com>
2026-01-26 09:50:14 +0600

    remove build output

--- .next/static/chunks/src_app_page_tsx_ff1f7b73._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_1edaf06c._.js",
-  "static/chunks/node_modules_cbe86239._.js"
-],
-    source: "dynamic"
-});
